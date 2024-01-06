3dffa0b209575f907fe6e55cf1e7127b8d37472e
mana0829 <marktacktaisuke@gmail.com>
2024-01-07 02:12:01 +0900

    取得データを追加したtextboxに表示

--- api-estat.py
+++ api-estat.py
@@ -1,14 +1,17 @@
 # -*- coding; utf-8 -*-
 """
 OpenWeatherMap からデータを取得する
-rev 0.0 クジラ飛行机,増補改訂Pythonによるスクレイピング＆機械学習 開発テクニック 初版,ソシム（株）,2019
+rev 0.0 クジラ飛行机,増補改訂Pythonによるスクレイピング&機械学習 開発テクニック 初版,ソシム（株）,2019
 rev 0.1 入力フォーム追加 by M.Nakano
 rev 0.2 apikey変更 by M.Nakano
+rev 0.3 textbox追加し、これに結果表示
 """
 import requests
-import json, pprint
-from tkinter import *
-from tkinter import ttk
+import json
+
+# tkinterのインポート
+import tkinter as tk
+import tkinter.ttk as ttk
 
 # APIキーの指定 - 以下を書き換えてください★ --- (※1)
 apikey = "556c2dd7923f2653ad118d89d374bf5b"
@@ -31,22 +34,34 @@ def get_waether():
     # 結果はJSON形式なのでデコードする --- (※7)
     data = json.loads(r.text)
     # print(data) 
+    text_area.delete(1.0, tk.END)
+    name = data["name"]
+    weather = data["weather"][0]["description"]
+    temp_min = k2c(data["main"]["temp_min"])
+    temp_max = k2c(data["main"]["temp_max"])
+    humidity = data["main"]["humidity"]
+    pressure = data["main"]["pressure"]
+    wind_speed = data["wind"]["speed"]
     # 結果を画面に表示 --- (※8)
-    print("+ 都市=", data["name"])
-    print("| 天気=", data["weather"][0]["description"])
-    print("| 最低気温=", k2c(data["main"]["temp_min"]))
-    print("| 最高気温=", k2c(data["main"]["temp_max"]))
-    print("| 湿度=", data["main"]["humidity"])
-    print("| 気圧=", data["main"]["pressure"])
-    print("| 風速度=", data["wind"]["speed"])
-    print("")
+    # テキストエリアに入力情報を表示
+    info = f"""
+都市: {name}
+天気: {weather}
+最低気温: {temp_min}
+最高気温: {temp_max}
+湿度: {humidity}
+気圧: {pressure}
+風速度: {wind_speed}
+"""
+    text_area.insert(tk.END, info)
+
 
 
 #Comboboxの選択項目用リスト
 cb_city_name = ['Tokyo,JP', 'London,UK', 'New York,US']
 
 #ウィンドウを作成
-app = Tk()
+app = tk.Tk()
 
 #ウィンドウサイズを指定
 app.geometry("320x240")
@@ -54,25 +69,32 @@ app.geometry("320x240")
 #ウィンドウタイトルを指定
 app.title('天気')
 
-frame1 = ttk.Frame(app, padding=(32))
-frame1.grid()
+frame1 = ttk.Frame(app)
+frame1.pack()
 
 #都市
 # Combobox
-city_name = StringVar()
+city_name = tk.StringVar()
 city_name_cb = ttk.Combobox(
     frame1, 
     textvariable=city_name, 
     values=cb_city_name, 
-    width=20)
+    width=30)
 city_name_cb.set(cb_city_name[0])
 city_name_cb.bind(
     '<<ComboboxSelected>>')
-city_name_cb.grid(row=0, column=1)
+# city_name_cb.grid(row=0, column=0)
+city_name_cb.pack()
 
 # Button
 button1 = ttk.Button(frame1, text='OK', command=get_waether)
-button1.grid(row=1, column=1)
+# button1.grid(row=1, column=0)
+button1.pack()
+
+# テキストエリアを作成
+text_area = tk.Text(frame1, height=9, width=50)
+# text_area.grid(row=2, column=0)
+text_area.pack()
 
 #ウィンドウ表示継続
 app.mainloop()
